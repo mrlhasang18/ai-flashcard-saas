8c820c028ebfcc41cad3cf220575cd380e4d59e2
Lhasang Tulku Lama <lamalhasang256@gmail.com>
2024-08-21 17:51:10 +0545

    generate & flashcards added

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
+'use client'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getFirestore} from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD4KxTWG4diWlaiBSJWtPclcuJK87RRSy8",
   authDomain: "ai-flashcard-saas-fea49.firebaseapp.com",
@@ -19,3 +17,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app)
+
+if (typeof window !== 'undefined') {
+  const analytics = getAnalytics(app);
+}
+
+export {db};
--- src/app/landingpage/landing.tsx
+++ src/app/landingpage/landing.tsx
@@ -5,6 +5,7 @@ import ToggleButton from "@/components/toggletheme";
 import FeatureSection from "@/components/features-section";
 import BackgroundGradientCard from "@/components/background-gradient-card";
 import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 
 export default function Landing() {
@@ -23,7 +24,6 @@ export default function Landing() {
     ]
   }
   `
-    const [output, setOutput] = useState('Click here to generate flashcards for your superhero')
     const generateText = async ()=>{
       try{
         const response = await fetch("/api/generate", {
@@ -64,18 +64,18 @@ export default function Landing() {
           Know your favourite<br /> superhero
         </motion.h1>
         
-        
+        <Link href="/generate">
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             Generate Flashcard
           </span>
         </button>
+        </Link>
           <FeatureSection/>
         <BackgroundGradientCard/>
         
         </LampContainer>
-        <p onClick={generateText}>{output}</p>
         
       </div>
     );
